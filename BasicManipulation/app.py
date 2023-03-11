5728fa3ccc6343c2fa950771dafeb59537963545
Aaronsupa <johnsona23@greenhill.org>
2023-03-11 10:58:45 -0600

    Updates

--- BasicManipulation/app.py
+++ BasicManipulation/app.py
@@ -1,8 +1,13 @@
 from flask import Flask, render_template
 app = Flask(__name__)
+menu = {0: 'Tacos', 1: 'Fajitas', 2: 'Enchiladas'}
+types = {0: ['Beef', 'Chicken', 'Brisket'], 1: ['Chicken', 'Cheese'], 2: ['Beef', 'Chicken', 'Cheese and Cream']}
 @app.route('/')
 @app.route('/shop')
 def shop():
-    menu = ['Taco', 'Fajita', 'Chips']
-    return render_template("shop.html", menu = menu)
+    shopTitle = "Taco Shop"
+    return render_template("shop.html", menu = menu, shopTitle = shopTitle)
 
+@app.route("/index/<int:id>")
+def index(id):
+    return render_template("index.html", menu = menu[id], types = types[id])
