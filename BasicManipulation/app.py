db08a71c691095c414260f0bcd011f037f64d26b
Aaronsupa <johnsona23@greenhill.org>
2023-03-14 14:06:48 -0500

    Added table to nutrition.html

--- BasicManipulation/app.py
+++ BasicManipulation/app.py
@@ -7,6 +7,7 @@ app.config["SECRET_KEY"] = "my_secret"
 
 menu = {0: 'Tacos', 1: 'Fajitas', 2: 'Enchiladas'}
 types = {0: ['Beef', 'Chicken', 'Brisket'], 1: ['Chicken', 'Cheese'], 2: ['Beef', 'Chicken', 'Cheese and Cream']}
+Cals = {0: 200, 1: 350, 2: 300}
 
 @app.route('/')
 @app.route('/shop')
@@ -17,3 +18,7 @@ def shop():
 @app.route("/index/<int:id>")
 def index(id):
     return render_template("index.html", menu = menu[id], types = types[id])
+
+@app.route("/nutrition")
+def nutrition():
+    return render_template("nutrition.html", menu = menu, types = types, Cals = Cals)
